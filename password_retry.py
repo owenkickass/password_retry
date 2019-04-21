33168483e4229c3bf5ad3f8417e67136d9dc17c2
Owen Chiu <q0972016679@gmail.com>
2019-04-21 22:42:31 +0900

    version 2

--- password_retry.py
+++ password_retry.py
@@ -1,6 +1,6 @@
 password = 'a123456'
 i = 3
-while True:
+while i > 0 :
 	pwd = input('請輸入密碼')
 	if pwd == password:
 		print('密碼正確')
@@ -8,9 +8,8 @@ while True:
 	else :
 		i = i-1
 		print('密碼錯誤 還剩', i ,'次機會')
-		if i < 1:
-			break
-			
+		
+
 
 
 
